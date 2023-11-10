6c3b3290e00f7e1db65d33a39724cb49e214f43b
MariCruz <maricruzdev@gmail.com>
2023-11-09 22:35:36 -0300

    first commit

---
+++ atv.js
@@ -0,0 +1,38 @@
+
+  /* 1-Função tradicional sem parâmetros
+function saudacao() {
+  console.log("Bem-vindo à calculadora!");
+  }
+saudacao()*/
+
+/* 2-Função tradicional com parâmetros e retorno de valor
+function calcular(operacao, num1, num2) {
+  let resultado;
+  if (operacao === "soma") {
+    resultado = num1 + num2;
+  } else if (operacao === "subtracao") {
+    resultado = num1 - num2;
+  } else if (operacao === "multiplicacao") {
+    resultado = num1 * num2;
+  } else if (operacao === "divisao") {
+    if (num2 === 0) {
+      resultado = "Não é possível dividir por zero.";
+    } else {
+      resultado = num1 / num2;
+    }
+  } else {
+    resultado = "Operação inválida";
+  }
+  return resultado;
+}
+console.log(calcular("soma", 4, 2))*/
+
+// 3-um arrow funcion com parametros e que retorne um valor
+
+/*const multiplicacao = (num1, num2) => num1*num2
+console.log(multiplicacao(2,4)) */
+
+
+
+
+
